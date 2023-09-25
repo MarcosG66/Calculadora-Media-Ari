44b2c088a3694b08e102e7020e42d5dd62f30dae
Marcos Gentili <marcosgentili66@gmail.com>
2023-09-25 01:13:09 -0300

    1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Calculator from './Calculator.jsx'
-import './index.css'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 )
